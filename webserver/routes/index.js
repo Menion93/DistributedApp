056c8b1a3f54d488e3afa3b4319a260a788f3602
miriana <miriana.mancini29@gmail.com>
2016-04-19 21:22:57 +0200

    first version views

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var controller = require('../controller/controller');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/ingredients', controller.getIngredients);
+
+router.get('/pizzas', controller.getPizzas);
+
+router.get('/newIngredient', controller.createIngredient);
+
+router.get('/newPizza', controller.createPizza);
+
+router.post('/save_ingredient', controller.showSavedIngredient);
+
+router.post('/save_pizza', controller.showSavedPizza);
+
 
 module.exports = router;
