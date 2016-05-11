1a324b8593319e217aa551ddcbcd5400a519a15c
miriana <miriana.mancini29@gmail.com>
2016-05-11 15:39:46 +0200

    start server

--- environment/script/ws-conf.sh
+++ environment/script/ws-conf.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y npm;
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 npm install /home/vagrant/webserver
 npm install forever -g
-forever start /home/vagrant/webserver/bin/www
+
 
 echo "--------------------------------"
 echo "WebServer Configuration Complete"
