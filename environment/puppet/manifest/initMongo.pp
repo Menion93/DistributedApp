f830b7286a4ef26a20ec9b9786d242a604b964ab
miriana <miriana.mancini29@gmail.com>
2016-05-16 00:01:41 +0200

    change db configuration

--- environment/puppet/manifest/initMongo.pp
+++ environment/puppet/manifest/initMongo.pp
@@ -1,6 +1,6 @@
 class {'::mongodb::server':
   port    => 27018,
   verbose => true,
-  bind_ip =>  ['0.0.0.0'],
+  bind_ip =>  ['10.11.1.201'],
   dbpath => '/home/data',
 }
--- environment/script/db-conf.sh
+++ environment/script/db-conf.sh
@@ -3,7 +3,6 @@ sudo rm -r /etc/puppet/modules
 mkdir -p /etc/puppet/modules;
 puppet module install puppetlabs-mongodb;
 mkdir /home/data;
-sudo cp /home/vagrant/data/* /home/data
 
 echo "-------------------------------"
 echo "Database Configuration Complete"
