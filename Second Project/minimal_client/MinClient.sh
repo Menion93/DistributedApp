798bbb41d1e945a49ec271b01ee9e8687f622c10
Miriana <miriana@MacBook-Pro-di-Emanuele.local>
2016-05-26 22:27:55 +0200

    change name

--- Second Project/minimal_client/MinClient.sh
+++ Second Project/minimal_client/MinClient.sh
@@ -2,7 +2,7 @@ echo "------------------------------------------------------"
 echo 
 echo "Adding an ingredient"
 echo 
-curl -s -X POST $(docker-machine ip default2):8000/ingredients/addbyname/pomodoro
+curl -s -X POST $(docker-machine ip default):8000/ingredients/addbyname/pomodoro
 echo 
 echo 
 echo "------------------------------------------------------"
@@ -10,7 +10,7 @@ echo "------------------------------------------------------"
 echo 
 echo "Get the ingredient"
 echo 
-curl -s -X GET $(docker-machine ip default2):8000/ingredients/findbyname/pomodoro
+curl -s -X GET $(docker-machine ip default):8000/ingredients/findbyname/pomodoro
 echo 
 echo 
 echo "------------------------------------------------------"
@@ -18,7 +18,7 @@ echo "------------------------------------------------------"
 echo 
 echo "Updating the ingredient"
 echo 
-curl -s -X PUT $(docker-machine ip default2):8000/ingredients/updatebyname/pomodoro/mozzarella
+curl -s -X PUT $(docker-machine ip default):8000/ingredients/updatebyname/pomodoro/mozzarella
 echo 
 echo 
 echo "------------------------------------------------------"
@@ -26,7 +26,7 @@ echo "------------------------------------------------------"
 echo 
 echo "Get the updated ingredient"
 echo 
-curl -s -X GET $(docker-machine ip default2):8000/ingredients/findbyname/mozzarella
+curl -s -X GET $(docker-machine ip default):8000/ingredients/findbyname/mozzarella
 echo 
 echo 
 echo "------------------------------------------------------"
@@ -34,7 +34,7 @@ echo "------------------------------------------------------"
 echo 
 echo "You cannot find the old ingredient anymore"
 echo 
-curl -s -X GET $(docker-machine ip default2):8000/ingredients/findbyname/pomodoro
+curl -s -X GET $(docker-machine ip default):8000/ingredients/findbyname/pomodoro
 echo 
 echo 
 echo "------------------------------------------------------"
@@ -42,7 +42,7 @@ echo "------------------------------------------------------"
 echo 
 echo "Remove the ingredient"
 echo 
-curl -s -X DELETE $(docker-machine ip default2):8000/ingredients/remove/mozzarella
+curl -s -X DELETE $(docker-machine ip default):8000/ingredients/remove/mozzarella
 echo 
 echo 
 echo "------------------------------------------------------"
